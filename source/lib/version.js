0db88581bc1d17b710abc3c9e5160453051949b1
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-05 00:23:46 -0400

    Define chemical inventory fields for users
    * Define chemical inventory fields for users
    
    * Bump Spa Coach to v0.9.12
    
    * Test inventory field definitions

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.9.11';
-const VERSION_CODE = 101;
+const APP_VERSION = '0.9.12';
+const VERSION_CODE = 102;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
