6fbe8fca0f68b18472d986fe53736fa4661d0f73
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-12 21:15:31 -0400

    Bump Spa Coach to v0.10.6

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.10.5';
-const VERSION_CODE = 108;
+const APP_VERSION = '0.10.6';
+const VERSION_CODE = 109;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
