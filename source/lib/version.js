6a525dd130c3efdc039674b7faf4f0aac93c4ebb
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-12 22:01:20 -0400

    Release Spa Coach v0.10.8 with duplicate reminder protection

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.10.7';
-const VERSION_CODE = 110;
+const APP_VERSION = '0.10.8';
+const VERSION_CODE = 111;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
