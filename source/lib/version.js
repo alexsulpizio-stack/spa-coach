30a1bbc655e6be0ccc66b68fa4539daac141483c
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-12 22:14:21 -0400

    Clear stale retest reminders after newer tests

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.10.8';
-const VERSION_CODE = 111;
+const APP_VERSION = '0.10.9';
+const VERSION_CODE = 112;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
