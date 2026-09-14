95bc2fd656104bfe0a65fb4efc8acceb7d49dc74
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-13 21:48:16 -0400

    Make low-pH retest reminders reliable when leaving the app (#31)

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.10.10';
-const VERSION_CODE = 113;
+const APP_VERSION = '0.10.11';
+const VERSION_CODE = 114;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
