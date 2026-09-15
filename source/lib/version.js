65ec0e05210132a17d15c260cad0cfcdc9cdbfe5
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-15 17:42:38 -0400

    Bump phone release to v0.10.21

--- source/lib/version.js
+++ source/lib/version.js
@@ -1,6 +1,6 @@
 (() => {
-const APP_VERSION = '0.10.20';
-const VERSION_CODE = 123;
+const APP_VERSION = '0.10.21';
+const VERSION_CODE = 124;
 const CACHE_NAME = `spa-coach-phone-v${APP_VERSION}`;
 
 globalThis.SpaVersion = Object.freeze({ APP_VERSION, VERSION_CODE, CACHE_NAME });
